c940a244a5f150678fd644480411dc630c00eb98
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2022-04-28 22:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-import dropbox
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from PIL import Image
+import PIL.ImageOps
 
-class TransferData:
-    def __init__(self,token):
-        self.token = token
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 
-    def upload_files(self, file_from, file_to):
-        dbx = dropbox.Dropbox(self.token)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=7500, test_size=2500)
 
-        with open(file_from, 'rb') as f:
-            dbx.files_upload(f.read(), file_to)
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0
 
-def main():
-    acc_tok = "sl.BFw-GxP7rPHJ12gbh76JI7u_HsHCJRvLgqXX3AaHpbv2UQcDgerS_mKwcU-_zKk4aeEOCXhJDb8nD2q8JUSDY6eIMKHYPAsMxTsd1_0aEuBIrCYJeGDLa_sVPtWxvo9qdgkrCMsrSGG2"
-    file_from="sample.txt"
-    file_to = "/Automate/sample.txt"
+clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
 
-    transferData = TransferData(acc_tok)
-    transferData.upload_files(file_from,file_to)
-    print("Transfer successful")
-
-main()
+def get_prediction(image):
+    im_pil = Image.open(image)
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
+    pixel_filter = 20
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+    max_pixel = np.max(image_bw_resized)
+    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+    test_pred = clf.predict(test_sample)
+    return test_pred[0]
